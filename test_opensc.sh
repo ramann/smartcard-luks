20494e63b97607c6a63138498db3995ed909bb03
ramann <robert@robeverybody.com>
2015-07-29 11:40:50 -0400

    wording

--- test_opensc.sh
+++ test_opensc.sh
@@ -13,13 +13,13 @@ pkcs15-init $verbose -G rsa/2048 -i 01 -a 01 -u sign,decrypt --pin 1234
 echo "export public key"
 pkcs15-tool --read-public-key 01 -o public_key_rsa2048.pem
 
-echo "create a basic message"
+echo "create a simple file"
 echo "This is just some plain text." > plain.txt
 
 echo "hash the file"
 openssl dgst -sha1 -binary -out plain.txt.sha1 < plain.txt
 
-echo "sign the file"
+echo "sign the hash"
 pkcs15-crypt $verbose -s --sha-1 --pkcs1 -i plain.txt.sha1 -o plain.txt.sign
 
 echo "verify the signature"
