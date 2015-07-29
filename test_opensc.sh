da695f82a6559c1823a2796f875dfed3c256b3a2
ramann <robert@robeverybody.com>
2015-07-29 11:03:26 -0400

    rsa 2048, sha1 hash

--- test_opensc.sh
+++ test_opensc.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
+#verbose='-vvvvvvvvv'
 
+echo "erase card"
 pkcs15-init -E
-pkcs15-init -vvvvvvvvv --create-pkcs15 -p pkcs15+onepin
-pkcs15-init -vvvvvvvvv -G rsa/1024 -i 45 -a 01 -u sign,decrypt --pin 1234
-pkcs15-tool --read-public-key 45 -o public_key_rsa1024.pem
+
+echo "create pkcs15 structure"
+pkcs15-init $verbose --create-pkcs15 -p pkcs15+onepin
+
+echo "create public/private keys"
+pkcs15-init $verbose -G rsa/2048 -i 01 -a 01 -u sign,decrypt --pin 1234
+
+echo "export public key"
+pkcs15-tool --read-public-key 01 -o public_key_rsa2048.pem
+
+echo "create a basic message"
 echo "This is just some plain text." > plain.txt
-openssl dgst -md5 -binary -out plain.txt.md5 < plain.txt
-pkcs15-crypt -vvvvvvvvv -s --md5 --pkcs1 -i plain.txt.md5 -o plain.txt.sign
-openssl dgst -verify puk.rsa.1024.pem -md5 -signature plain.txt.sign < plain.txt
-openssl rsautl -pubin -inkey puk.rsa.1024.pem -encrypt -in plain.txt -out plain.txt.enc
-pkcs15-crypt -vvvvvvvvv -c --pkcs1 -i plain.txt.enc -o plain.txt.dec
+
+echo "hash the file"
+openssl dgst -sha1 -binary -out plain.txt.sha1 < plain.txt
+
+echo "sign the file"
+pkcs15-crypt $verbose -s --sha-1 --pkcs1 -i plain.txt.sha1 -o plain.txt.sign
+
+echo "verify the signature"
+openssl dgst -verify public_key_rsa2048.pem -sha1 -signature plain.txt.sign < plain.txt
+
+echo "encrypt the file"
+openssl rsautl -pubin -inkey public_key_rsa2048.pem -encrypt -in plain.txt -out plain.txt.enc
+
+echo "decrypt the file"
+pkcs15-crypt $verbose -c --pkcs1 -i plain.txt.enc -o plain.txt.dec
 
 
